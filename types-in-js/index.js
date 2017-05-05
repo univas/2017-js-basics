e126809c04727310c9e9f516592464ccc5e86186
Edy Segura <edysegura@gmail.com>
2017-05-04 22:09:03 -0300

    Added types example

---
+++ types-in-js/index.js
@@ -0,0 +1,27 @@
+/**
+ * Variable types in JavaScript
+ */
+
+//null
+let novalue = null
+console.log("No value: " + novalue)
+console.log("Type: " + typeof novalue)
+console.log("-----------------------------")
+
+//String
+let name = "Nilton"
+console.log("Name: " + name)
+console.log("Type: " + typeof name)
+
+//Number
+let age = 21
+
+//Boolean
+let isMale = true
+
+//Object
+let person = {
+    name: "Tulio",
+    email: "tulio@univas.edu.br"
+}
+
